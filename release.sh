c51dfb0b54af51b2c0fb7e4d9bdce830736860b4
Takuenn Li <qingcaomc@gmail.com>
2024-09-03 21:20:58 +0800

    ci: add target darwin_arm64 (#2)

--- release.sh
+++ release.sh
@@ -6,7 +6,8 @@ function pack() {
 }
 
 GOOS=linux go build -ldflags "-w -s -X 'main.version=$1'" -o release/linux/elise cmd/elise/main.go
-GOOS=darwin go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin/elise cmd/elise/main.go
+GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin/elise_amd64 cmd/elise/main.go
+GOOS=darwin GOARCH=arm64 go build -ldflags "-w -s -X 'main.version=$1'" -o release/darwin/elise_arm64 cmd/elise/main.go
 GOOS=windows go build -ldflags "-w -s -X 'main.version=$1'" -o release/windows/elise.exe cmd/elise/main.go
 
 cd release || exit
